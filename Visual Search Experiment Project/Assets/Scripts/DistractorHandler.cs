b194457dab860a5d3c04837167b68fa88af4e92f
Tom Peney <tompeney@hotmail.co.uk>
2021-11-12 13:45:51 +0000

    Locations now just transforms
    Changed the distractors from toggling gameobjects on/off to being set positions and instantiating the given distractor models with additional parameter options.

--- Visual Search Experiment Project/Assets/Scripts/DistractorHandler.cs
+++ Visual Search Experiment Project/Assets/Scripts/DistractorHandler.cs
@@ -6,47 +6,67 @@ using System.Linq;
 public class DistractorHandler : MonoBehaviour
 {
 
-    public int numDistractors;
+    public int numDistractors; // to-do - Set from current trial
+    public Distractor[] distractorTypes;
 
-
+    // Class for storing distractor parameters
+    [System.Serializable]
+    public class Distractor
+    {
+        public GameObject model;
+        public int frequency;
+        public float rotation;
+    }
+     
     // Start is called before the first frame update
     void Start()
     {
 
        // Create list of Children GameObjects - Each Distractor
-        GameObject[] distractors = new GameObject[transform.childCount];
+        GameObject[] distractorLocations = new GameObject[transform.childCount];
         int i = 0;
         foreach (Transform child in transform)
         {
-            distractors[i] = child.gameObject;
+            distractorLocations[i] = child.gameObject;
             i += 1;
         }
 
-        int allDistractors = distractors.Length;
+        int allDistractors = distractorLocations.Length;
 
         // Hide all distractors at start 
-        foreach (GameObject distractor in distractors)
-        {
-            distractor.SetActive(false);
-        }
+        //foreach (GameObject distractor in distractorLocations)
+        //{
+        //    distractor.SetActive(false);
+        //}
 
         for (int t = 0; t < allDistractors; t++)
         {
-            GameObject tmp = distractors[t];
+            GameObject tmp = distractorLocations[t];
             int r = Random.Range(t, allDistractors);
-            distractors[t] = distractors[r];
-            distractors[r] = tmp;
+            distractorLocations[t] = distractorLocations[r];
+            distractorLocations[r] = tmp;
         }
 
-        for (int d = 0; d < numDistractors; d++)
+        //for (int d = 0; d < numDistractors; d++)
+        //{
+        //    distractorLocations[d].SetActive(true);
+        //}
+
+        int spawnCount = 0;
+        foreach (Distractor distractor in distractorTypes)
         {
-            distractors[d].SetActive(true);
+            for (int rep = 0; rep < distractor.frequency; rep++)
+            {
+                Instantiate(distractor.model, distractorLocations[spawnCount].transform); //set quaternion
+
+                spawnCount++;
+            }
         }
+
 
        }
- 
 
-    
+
 
     // Update is called once per frame
     void Update()
